de69b0bc87c58a65624bd2640735de1b2c833555
Thanh Nguyen <thanhn1756@gmail.com>
2021-01-13 23:15:29 -0800

    updated Group.js with Modal

---
+++ client/src/components/modules/Group.js
@@ -0,0 +1,52 @@
+
+
+//TODO: install semantic UI
+//
+
+import React from "react";
+import { Button, Header, Image, Modal } from "semantic-ui-react";
+
+const ModalExampleModal = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Modal
+      onClose={() => setOpen(false)}
+      onOpen={() => setOpen(true)}
+      open={open}
+      dimmer="blurring"
+      trigger={<Button>Show Modal</Button>}
+    >
+      <Modal.Header>Select a Photo</Modal.Header>
+      <Modal.Content image>
+        <Image
+          size="medium"
+          src="https://react.semantic-ui.com/images/avatar/large/rachel.png"
+          wrapped
+        />
+        <Modal.Description>
+          <Header>Default Profile Image</Header>
+          <p>
+            We've found the following gravatar image associated with your e-mail
+            address. sdfsdfs dsd f
+          </p>
+          <p>Is it okay to use this photo?</p>
+        </Modal.Description>
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="black" onClick={() => setOpen(false)}>
+          Nope
+        </Button>
+        <Button
+          content="Yep, that's me"
+          labelPosition="right"
+          icon="checkmark"
+          onClick={() => setOpen(false)}
+          positive
+        />
+      </Modal.Actions>
+    </Modal>
+  );
+}
+
+export default ModalExampleModal;
