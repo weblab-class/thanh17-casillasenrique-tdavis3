8e31be48b667fd0f1c8041a46aed324a834fa515
Enrique <enriquec@mit.edu>
2021-01-19 00:56:50 -0500

    tiny bug fix in group

--- client/src/components/modules/Group.js
+++ client/src/components/modules/Group.js
@@ -30,7 +30,7 @@ const Group = ({ bookmarks, inEditMode, userId, name }) => {
       trigger={
         <CollapsedGroup
           name={name}
-          bookmarkIcons={bookmarks.map((bookmark) => bookmark.image)}
+          bookmarkIcons={bookmarks.map((bookmark) => { return (bookmark.customIcon) ? URL.createObjectURL(bookmark.customIcon) : bookmark.icon})}
           onClick={() => setOpen(true)}
         />
       }
