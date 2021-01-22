3bfd0d51b0c47326b3365258c280cce96615909b
Thanh Nguyen <thanhn1756@gmail.com>
2021-01-22 03:27:18 -0800

    reformated code

--- client/src/components/modules/Bookmark.js
+++ client/src/components/modules/Bookmark.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect, useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./Bookmark.css";
 import "../../utilities.css";
-import {Button, Menu, Popup} from "semantic-ui-react";
+import { Button, Menu, Popup } from "semantic-ui-react";
 import globe from "../../public/images/globe.png";
 import { useDrag } from "react-dnd";
 import { ItemTypes } from "../pages/Home";
@@ -20,117 +20,110 @@ const FAVICON_URL = "https://www.google.com/s2/favicons?sz=256&domain_url=";
  * @returns {JSX.Element}
  * @constructor
  */
-const Bookmark = ({userId,inEditMode, url, name, icon, customIcon, index, onRemove, _id}) => {
+const Bookmark = ({ userId, inEditMode, url, name, icon, customIcon, index, onRemove, _id }) => {
+  const contextRef = useRef();
+  const [open, setOpen] = useState(false);
+  const [displayedIcon, setDisplayedIcon] = useState(globe);
 
-    const contextRef = useRef()
-    const [open, setOpen] = useState(false)
-    const [displayedIcon, setDisplayedIcon] = useState(globe);
-
-    useEffect(() => {
-        if (customIcon) {
-            //console.log("the custom icon object (should not a binary file) in the bookmark: " + customIcon + " " + name);
-            setDisplayedIcon(customIcon);
-        } else {
-            setDisplayedIcon(icon);
-        }
-    });
-
-    useEffect(() => {
-        watchBookmark();
-    }, [inEditMode]);
-
-    const watchBookmark = () => {
-    };
-
-    function createContextFromEvent(e) {
-        const left = e.clientX
-        const top = e.clientY
-        const right = left + 1
-        const bottom = top + 1
-
-        return {
-            getBoundingClientRect: () => ({
-                left,
-                top,
-                right,
-                bottom,
-
-                height: 0,
-                width: 0,
-            }),
-        }
+  useEffect(() => {
+    if (customIcon) {
+      //console.log("the custom icon object (should not a binary file) in the bookmark: " + customIcon + " " + name);
+      setDisplayedIcon(customIcon);
+    } else {
+      setDisplayedIcon(icon);
     }
+  });
+
+  useEffect(() => {
+    watchBookmark();
+  }, [inEditMode]);
+
+  const watchBookmark = () => {};
+
+  function createContextFromEvent(e) {
+    const left = e.clientX;
+    const top = e.clientY;
+    const right = left + 1;
+    const bottom = top + 1;
+
+    return {
+      getBoundingClientRect: () => ({
+        left,
+        top,
+        right,
+        bottom,
+
+        height: 0,
+        width: 0,
+      }),
+    };
+  }
 
   /**
    * Turns a Bookmark into a dragable component
    */
-  const [{isDragging}, drag] = useDrag({
-        item: {
-          type: ItemTypes.BOOKMARK,
-          _id: _id,
-          // customRow: customRow,
-          // customCol: customCol,
-          index: index
-        },
-        collect: monitor => ({
-            isDragging: !!monitor.isDragging(),
-        }),
-    })
-    return (
-        <div>
-            <form action={"http://"+url.replace(new RegExp("((http|https)://)?(www.)?"),"")} target="_blank">
-                <button disabled={inEditMode} className="Bookmark-button u-flex-alignCenter" type="submit"
-                        onContextMenu={(e) => {
-                            e.preventDefault()
-                            contextRef.current = createContextFromEvent(e)
-                            setOpen(true)
-                        }}>
-                        <img ref={drag}
-                             style={{
-                               opacity: isDragging ? 0 : 1,
-                               fontSize: 25,
-                               fontWeight: 'bold',
-                               cursor: isDragging? 'grabbing': "grab",
-                               borderRadius: "20%"
-                             }}
-                             className="Bookmark-image u-flex-alignCenter u-grow" src={displayedIcon}/>
-                        {/*<div className="Bookmark-text-container u-flex-alignCenter">*/}
+  const [{ isDragging }, drag] = useDrag({
+    item: {
+      type: ItemTypes.BOOKMARK,
+      _id: _id,
+      // customRow: customRow,
+      // customCol: customCol,
+      index: index,
+    },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+  return (
+    <div>
+      <form
+        action={"http://" + url.replace(new RegExp("((http|https)://)?(www.)?"), "")}
+        target="_blank"
+      >
+        <button
+          disabled={inEditMode}
+          className="Bookmark-button u-flex-alignCenter"
+          type="submit"
+          onContextMenu={(e) => {
+            e.preventDefault();
+            contextRef.current = createContextFromEvent(e);
+            setOpen(true);
+          }}
+        >
+          <img
+            ref={drag}
+            style={{
+              opacity: isDragging ? 0 : 1,
+              fontSize: 25,
+              fontWeight: "bold",
+              cursor: isDragging ? "grabbing" : "grab",
+              borderRadius: "20%",
+            }}
+            className="Bookmark-image u-flex-alignCenter u-grow"
+            src={displayedIcon}
+          />
+          {/*<div className="Bookmark-text-container u-flex-alignCenter">*/}
 
-                        {/*</div>*/}
-
-                </button>
-                <p className="Bookmark-text u-bold "
-                  style={{opacity: isDragging?0:1 }}>{name}</p>
-            </form>
-            <Popup
-                basic
-                context={contextRef}
-                onClose={() => setOpen(false)}
-                open={open}
-            >
-                <Menu
-                    items={[
-                        { key: 'delete', content: 'Delete', icon: 'remove' },
-                    ]}
-                    onItemClick={() => {
-                        onRemove();
-                        setOpen(false);
-                    }}
-                    secondary
-                    vertical
-                />
-            </Popup>
-            {(inEditMode) &&
-            <Button
-                size="tiny"
-                inverted
-                circular
-                icon="close"
-                onClick={onRemove}
-            />
-            }
-        </div>
-    );
+          {/*</div>*/}
+        </button>
+        <p className="Bookmark-text u-bold " style={{ opacity: isDragging ? 0 : 1 }}>
+          {name}
+        </p>
+      </form>
+      <Popup basic context={contextRef} onClose={() => setOpen(false)} open={open}>
+        <Menu
+          items={[{ key: "delete", content: "Delete", icon: "remove" }]}
+          onItemClick={() => {
+            onRemove();
+            setOpen(false);
+          }}
+          secondary
+          vertical
+        />
+      </Popup>
+      {inEditMode && <Button size="tiny" inverted circular icon="close" onClick={onRemove} />}
+    </div>
+  );
 };
 
 export default Bookmark;
--- client/src/components/modules/IconSelect.js
+++ client/src/components/modules/IconSelect.js
@@ -1,13 +1,13 @@
-import React, {useState, useEffect} from "react";
-import {Loader, Icon, Menu} from "semantic-ui-react";
+import React, { useState, useEffect } from "react";
+import { Loader, Icon, Menu } from "semantic-ui-react";
 import standardIcon from "../../public/images/globe.png";
 import fileUpload from "../../public/images/fileUpload.png";
 import "./IconSelect.css";
 
 const defaultIconLoader = (
-    <Loader className="ui active centered inline" size="small">
-        <p className="IconSelect-default-loader">Enter a URL</p>
-    </Loader>
+  <Loader className="ui active centered inline" size="small">
+    <p className="IconSelect-default-loader">Enter a URL</p>
+  </Loader>
 );
 
 /**
@@ -17,86 +17,93 @@ const defaultIconLoader = (
  * @returns {JSX.Element} selections of icons to choose from for the bookmark
  * @constructor
  */
-const IconSelect = ({onSelect, defaultIcon}) => {
-    const [selected, setSelected] = useState("standard");
-    const [uploaded, setUploaded] = useState(false);
+const IconSelect = ({ onSelect, defaultIcon }) => {
+  const [selected, setSelected] = useState("standard");
+  const [uploaded, setUploaded] = useState(false);
 
-    useEffect(() => {
-        if (selected !== "upload") {
-            setUploaded(false)
-        }
-        if (selected === "default" && defaultIcon === undefined) {
-            console.log("changing selection as default url doesnt exist anymore")
-            onSelect(standardIcon, "ICON");
-            setSelected("standard");
-        }
-    }, [selected, defaultIcon]);
+  useEffect(() => {
+    if (selected !== "upload") {
+      setUploaded(false);
+    }
+    if (selected === "default" && defaultIcon === undefined) {
+      console.log("changing selection as default url doesnt exist anymore");
+      onSelect(standardIcon, "ICON");
+      setSelected("standard");
+    }
+  }, [selected, defaultIcon]);
 
-    return (
-        <div className="IconSelect-container">
-            <div className={"IconSelect-option" + (selected === "standard" ? "-selected" : "")}>
-                <div style={{
-                    display: "flex", alignItems: "center", justifyContent: "space-around",
-                    flexDirection: "column"
-                }}>
-                    <img
-                        className={"IconSelect-bookmarkImage"}
-                        src={standardIcon}
-                        onClick={() => {
-                            console.log("clicked on standard");
-                            onSelect(standardIcon, "ICON");
-                            setSelected("standard");
-                        }}
-                    />
-                </div>
-
-            </div>
-            <div>
-                {!defaultIcon ? (
-                    defaultIconLoader
-                ) : (
-                    <div className={"IconSelect-option" + (selected === "default" ? "-selected" : "")}>
-                        <img
-                            style={{
-                                width: "4vw",
-                                height: "4vw", borderRadius: "20%"
-                            }}
-                            src={defaultIcon}
-                            onClick={() => {
-                                console.log("clicked on default");
-                                onSelect(defaultIcon, "ICON");
-                                setSelected("default");
-                            }}
-                        />
-                    </div>
-                )}
-            </div>
-            <label
-                className={"IconSelect-option" + ((selected === "upload") ? "-selected" : "")}
-                onClick={() => {
-                    console.log("clicked on default");
-                    setSelected("upload");
-                    console.log(document.getElementById("fileElem").files[0]);
-                }}
-            >
-                <img
-                    src={(uploaded && document.getElementById("fileElem").files.length > 0) ? URL.createObjectURL(document.getElementById("fileElem").files[0]) : fileUpload}
-                    className="IconSelect-fileUploadImage"
-                />
-                <input
-                    type="file"
-                    id="fileElem"
-                    accept="image/*"
-                    className="IconSelect-invisibleInput"
-                    onChange={(event) => {
-                        onSelect(event.target.files[0], "FILE");
-                        setUploaded(true);
-                    }}
-                />
-            </label>
-
+  return (
+    <div className="IconSelect-container">
+      <div className={"IconSelect-option" + (selected === "standard" ? "-selected" : "")}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            className={"IconSelect-bookmarkImage"}
+            src={standardIcon}
+            onClick={() => {
+              console.log("clicked on standard");
+              onSelect(standardIcon, "ICON");
+              setSelected("standard");
+            }}
+          />
         </div>
-    );
+      </div>
+      <div>
+        {!defaultIcon ? (
+          defaultIconLoader
+        ) : (
+          <div className={"IconSelect-option" + (selected === "default" ? "-selected" : "")}>
+            <img
+              style={{
+                width: "4vw",
+                height: "4vw",
+                borderRadius: "20%",
+              }}
+              src={defaultIcon}
+              onClick={() => {
+                console.log("clicked on default");
+                onSelect(defaultIcon, "ICON");
+                setSelected("default");
+              }}
+            />
+          </div>
+        )}
+      </div>
+      <label
+        className={"IconSelect-option" + (selected === "upload" ? "-selected" : "")}
+        onClick={() => {
+          console.log("clicked on default");
+          setSelected("upload");
+          console.log(document.getElementById("fileElem").files[0]);
+        }}
+      >
+        <img
+          src={
+            uploaded && document.getElementById("fileElem").files.length > 0
+              ? URL.createObjectURL(document.getElementById("fileElem").files[0])
+              : fileUpload
+          }
+          className="IconSelect-fileUploadImage"
+        />
+        <input
+          type="file"
+          id="fileElem"
+          accept="image/*"
+          className="IconSelect-invisibleInput"
+          onChange={(event) => {
+            onSelect(event.target.files[0], "FILE");
+            setUploaded(true);
+          }}
+        />
+      </label>
+    </div>
+  );
 };
 
 export default IconSelect;
