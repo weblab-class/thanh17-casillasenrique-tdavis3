237835f6a6eaaaf88bbefe30b53b408a73e4fae5
Tyrone Davis III <td3@mit.edu>
2021-01-24 23:06:32 -0500

    updated User model for settings fields

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
+  backgroundImage: 'Buffer',
+  isDarkMode: Boolean
 });
 
 // compile model from schema
